310076b25416cf7746b750b4dd71f0703074289f
Kosisochukwu Nzeako <kosi@okra.ng>
2023-02-16 09:50:28 +0100

    upgraded webview

--- lib/view/web.dart
+++ lib/view/web.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:okra_widget_official/models/okra_handler.dart';
 import 'package:okra_widget_official/raw/okra_html.dart';
@@ -10,7 +8,7 @@ import 'package:webview_flutter/webview_flutter.dart';
 class Web extends StatefulWidget {
   final Map<String, dynamic>? okraOptions;
   final String? shortUrl;
-  final useShort;
+  final bool useShort;
   final Function(String data)? onSuccess;
   final Function(String message)? onError;
   final Function(String message)? onClose;
@@ -20,7 +18,7 @@ class Web extends StatefulWidget {
       {Key? key,
       this.okraOptions,
       this.shortUrl,
-      this.useShort,
+      required this.useShort,
       this.onError,
       this.beforeClose,
       this.onClose,
@@ -32,85 +30,69 @@ class Web extends StatefulWidget {
 }
 
 class _WebState extends State<Web> {
-  WebViewController? _controller;
+  late WebViewController _controller;
   bool isLoading = true;
   OkraHandler okraHandler =
       new OkraHandler(false, false, false, true, false, "");
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      WebView(
+    void onFlutterSuccess(JavaScriptMessage message) {
+      if (widget.onSuccess != null) {
+        widget.onSuccess!(message.message);
+      }
+    }
 
-          // initialUrl: "https://mobile.okra.ng/",
-          initialUrl: widget.useShort
-              ? Uri.dataFromString(
-                  buildOkraWidgetWithShortUrl(
-                    widget.shortUrl,
-                  ),
-                  mimeType: 'text/html',
-                ).toString()
-              : Uri.dataFromString(
-                  mBuildOkraWidgetWithOptions(
-                    widget.okraOptions!,
-                  ),
-                  mimeType: 'text/html',
-                ).toString(),
-          onPageFinished: (response) {
-            setState(() {
-              isLoading = false;
-            });
-            // String jsonOptions = json.encode(widget.okraOptions);
-            // _controller.evaluateJavascript("openOkraWidget('$jsonOptions')");
-          },
-          javascriptMode: JavascriptMode.unrestricted,
-          gestureRecognizers: [
-            Factory(() => VerticalDragGestureRecognizer()),
-            Factory(() => TapGestureRecognizer()),
-          ].toSet(),
-          javascriptChannels: Set.from([
-            JavascriptChannel(
-                name: 'FlutterOnSuccess',
-                onMessageReceived: (JavascriptMessage message) {
-                  // okraHandler =
-                  //     new OkraHandler(true, true, false, false, false, message.message);
-                  if (widget.onSuccess != null) {
-                    widget.onSuccess!(message.message);
-                  }
-                }),
-            JavascriptChannel(
-                name: 'FlutterOnError',
-                onMessageReceived: (JavascriptMessage message) {
-                  // okraHandler =
-                  //     new OkraHandler(true, false, true, false, false, jsonDecode(message.message)["data"]["error"].toString());
-                  if (widget.onError != null) {
-                    widget.onError!(jsonDecode(message.message)["data"]["error"]
-                        .toString());
-                  }
-                }),
-            JavascriptChannel(
-                name: 'FlutterOnClose',
-                onMessageReceived: (JavascriptMessage message) {
-                  // okraHandler =
-                  // new OkraHandler(true, false, false, true, false, message.message);
-                  if (widget.onClose != null) {
-                    widget.onClose!(message.message);
-                  }
-                  Navigator.pop(context);
-                }),
-            JavascriptChannel(
-                name: 'FlutterBeforeClose',
-                onMessageReceived: (JavascriptMessage message) {
-                  // okraHandler =
-                  // new OkraHandler(true, false, false, false, true, message.message);
-                  if (widget.beforeClose != null) {
-                    widget.beforeClose!(message.message);
-                  }
-                })
-          ]),
-          onWebViewCreated: (webViewController) {
-            _controller = webViewController;
-          }),
+    void onFlutterError(JavaScriptMessage message) {
+      if (widget.onError != null) {
+        widget.onError!(jsonDecode(message.message)["data"]["error"]
+            .toString());
+      }
+    }
+
+    void onFlutterClose(JavaScriptMessage message) {
+      if (widget.onClose != null) {
+        widget.onClose!(message.message);
+      }
+      Navigator.pop(context);
+    }
+
+    void onFlutterBeforeClose(JavaScriptMessage message) {
+      if (widget.beforeClose != null) {
+        widget.beforeClose!(message.message);
+      }
+    }
+
+    void onPageLoaded(String _) {
+      setState(() {
+        isLoading = false;
+      });
+    }
+
+    String getUrl(bool useShort) {
+      if(useShort) {
+        return
+          buildOkraWidgetWithShortUrl(
+            widget.shortUrl,
+          );
+
+      } else return
+        mBuildOkraWidgetWithOptions(
+          widget.okraOptions!,
+        );
+    }
+
+    print(getUrl(widget.useShort));
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..addJavaScriptChannel("FlutterOnSuccess", onMessageReceived: onFlutterSuccess)
+      ..addJavaScriptChannel("FlutterOnError", onMessageReceived: onFlutterError)
+      ..addJavaScriptChannel("FlutterOnClose", onMessageReceived: onFlutterClose)
+      ..addJavaScriptChannel("FlutterBeforeClose", onMessageReceived: onFlutterBeforeClose)
+      ..setNavigationDelegate( NavigationDelegate(onPageFinished: onPageLoaded) )
+      ..loadHtmlString(getUrl(widget.useShort));
+    return Stack(children: [
+      WebViewWidget(controller: _controller),
       isLoading
           ? Center(
               child: CircularProgressIndicator(),
